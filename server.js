94439d5cd4f075c22aa2346386d9a77229aee53a
Shubham28497 <shubhamgupta24640020@gmail.com>
2024-02-22 11:27:53 +0530

    update server file with env config

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
 const express = require("express");
+require("dotenv").config();
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const PORT = 5000;
 const app = express();
+
+//! Configure cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 app.listen(PORT, console.log(`Server is running on ${PORT}`));
